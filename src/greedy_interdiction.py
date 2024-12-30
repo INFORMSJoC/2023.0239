f13d27ada696658301966ccc3711d4fac122054f
Yasmine Beck <becky@uni-trier.de>
2024-12-30 16:47:39 +0100

    minor

--- src/greedy_interdiction.py
+++ src/greedy_interdiction.py
@@ -2,10 +2,10 @@
 # This file is part of the code used for the computational study #
 # in the paper                                                   #
 #                                                                #
-#     "Heuristic Methods for Gamma-Robust Mixed-Integer Linear   #
-#      Bilevel Problems"                                         #
+#  "Heuristic Methods for Gamma-Robust Mixed-Integer Linear      #
+#   Bilevel Problems"                                            #
 #                                                                #
-# by Yasmine Beck, Ivana Ljubic, and Martin Schmidt (2024).      #
+# by Yasmine Beck, Ivana Ljubic, and Martin Schmidt (2025).      #
 ##################################################################
 
 # Global imports
